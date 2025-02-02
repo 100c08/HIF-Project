9f932ed54c21159e5121cea913c6a7dd5cc3e7c2
100c08 <dldudrl3010@gmail.com>
2025-02-02 18:26:39 +0900

    섹션2 수정

--- src/components/Home/Section2.js
+++ src/components/Home/Section2.js
@@ -1,7 +1,25 @@
 import Image from 'next/image';
 import styles from '../../styles/Home/Section2.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Section2({ isActive }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 480);
+    };
+
+    // 초기 설정
+    handleResize();
+
+    // 리사이즈 이벤트 리스너 추가
+    window.addEventListener('resize', handleResize);
+
+    // 클린업
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.backgroundGradient}></div>
@@ -17,14 +35,19 @@ export default function Section2({ isActive }) {
               HUFS to Heights, Finance to Futures
             </h3>
             <p className={`${styles.introText} ${styles.fadeUpAnimation4}`}>
-              금융연구회는 2010년 설립된 한국외국어대학교<br />
+              금융연구회는 2010년 설립된 한국외국어대학교
+              {isMobile ? ' ' : <br />}
               경영대학 소속의 금융 전문 학회입니다
             </p>
             <p className={`${styles.detailText} ${styles.fadeUpAnimation5}`}>
-              우리 학회는 금융세미나, 신문스터디, 공모전 참가 등 다양한 활동을 통해 <br />
-              경제와 금융에 대한 폭넓은 지식을 쌓고, 선후배 간의 긴밀한 네트워크를 <br />
-              바탕으로 금융시장 동향 및 실무 인사이트를 학습합니다. <br />
-              이를 통해 구성원들은 전문성을 갖춘 <br />
+              우리 학회는 금융세미나, 신문스터디, 공모전 참가 등 다양한 활동을 통해
+              {isMobile ? ' ' : <br />}
+              경제와 금융에 대한 폭넓은 지식을 쌓고, 선후배 간의 긴밀한 네트워크를
+              {isMobile ? ' ' : <br />}
+              바탕으로 금융시장 동향 및 실무 인사이트를 학습합니다.
+              {isMobile ? ' ' : <br />}
+              이를 통해 구성원들은 전문성을 갖춘
+              {isMobile ? ' ' : <br />}
               금융 인재로 성장해 나갑니다.
             </p>
           </div>
