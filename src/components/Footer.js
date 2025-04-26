c4e536ea300d8c1549badd3a31dee537a797a4d5
100c08 <dldudrl3010@gmail.com>
2025-04-27 02:14:42 +0900

    25-1 중간 수정3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,13 +24,43 @@ export default function Footer() {
               <path fill="#999999" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
             </svg>
           </Link>
-          <Image
-            src="/White_O.svg"
-            alt="HIF Logo"
-            width={70}  // 로고 크기 증가
-            height={70}
-            className={styles.footerLogo}
-          />
+          <div className={styles.logoContainer}>
+            <div className={`${styles.logoWrapper} ${styles.left}`}>
+              <Image
+                src="/sb_img1.png"
+                alt="SB Logo"
+                width={140}
+                height={40}
+                className={styles.footerLogo}
+                style={{ 
+                  filter: 'brightness(0) invert(0.8)',
+                  objectFit: 'contain'
+                }}
+              />
+            </div>
+            <div className={`${styles.logoWrapper} ${styles.center}`}>
+              <Image
+                src="/White_O.svg"
+                alt="HIF Logo"
+                width={70}
+                height={70}
+                className={styles.footerLogo}
+                style={{ filter: 'brightness(0) invert(0.8)' }}
+              />
+            </div>
+            <div className={`${styles.logoWrapper} ${styles.right}`}>
+              <Image
+                src="/김준기문화재단.png"
+                alt="김준기문화재단 Logo"
+                width={180}
+                height={60}
+                className={`${styles.footerLogo} ${styles.dongbukLogo}`}
+                style={{ 
+                  objectFit: 'contain'
+                }}
+              />
+            </div>
+          </div>
           <div className={styles.copyright}>
             © 2025 HIF. All Rights Reserved.
           </div>
